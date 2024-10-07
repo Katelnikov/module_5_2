63a885e5fb40e3cb553fc4b57dc08fa9ab728ca4
Katelnikov <egor_katelnikov@mail.ru>
2024-10-07 19:38:39 +0300

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,38 @@
+class House: # Класс House
+    def __init__(self, name, number_of_floors): # метод(параметр)
+        self.name = 'ЖК Эльбрус' # атрибуты объекта
+        self.number_of_floors = number_of_floors # атрибуты объекта
+
+
+    def go_to(self, new_floor, ): # метод(параметр)
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+        else:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+
+    # Задача "Магические здания":
+    # Для решения этой задачи будем пользоваться решением к предыдущей задаче "Атрибуты и методы объекта".
+    # Необходимо дополнить класс House следующими специальными методами:
+    # __len__(self) - должен возвращать кол-во этажей здания self.number_of_floors.
+    # __str__(self) - должен возвращать строку: "Название: <название>, кол-во этажей: <этажи>".
+
+
+    def __len__(self):
+        return self.number_of_floors
+
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+
+h1 = House('ЖК Эльбрус',10)
+h2 = House('ЖК Акация', 20)
+
+# __len__
+print(len(h1))
+print(len(h2))
+
+# __str__
+print(h1)
+print(h2)
